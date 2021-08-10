8ca2f637f8da966e5df0815923b45e1152a1af76
kagurayayoi <862883646@qq.com>
2021-08-10 18:35:21 +0800

    Complete ChapterController (chapter&ex)

--- src/main/java/top/kagurayayoi/phidbapi/controller/ChapterController.java
+++ src/main/java/top/kagurayayoi/phidbapi/controller/ChapterController.java
@@ -49,6 +49,37 @@ public class ChapterController {
         }
     }
 
+    @GetMapping({"/api/chapter/ex/{ChapterName}", "/api/Chapter/Ex/{ChapterName}", "/api/Chapter/ex/{ChapterName}", "/api/chapter/Ex/{ChapterName}"})
+    @ResponseBody
+    public ResponseEntity<AjaxResult> ChapterEX(@PathVariable("ChapterName") String ChapterName){
+        this.Init();
+        result.setLocation("/api/chapter/ex/" + ChapterName);
+        try {
+            helper.connection();
+            ResultSet rs = helper.selectAll("main.'Chapter-Ex-" + ChapterName+ "'", null);
+            while (rs.next()){
+                GeneralEntity general = new GeneralEntity();
+                general.setId(rs.getInt(GeneralEntity.columnName[0]));
+                general.setName(rs.getString(GeneralEntity.columnName[1]));
+                general.setEZ(rs.getObject(GeneralEntity.columnName[2]));
+                general.setHD(rs.getObject(GeneralEntity.columnName[3]));
+                general.setIN(rs.getObject(GeneralEntity.columnName[4]));
+                general.setAT(rs.getObject(GeneralEntity.columnName[5]));
+                general.setLegacy(rs.getObject(GeneralEntity.columnName[6]));
+                list.add(general);
+            }
+            result.setResultObj(list);
+            helper.close();
+            return ResponseEntity.ok().body(result);
+        }catch (Exception ex){
+            result.setCode(HttpStatus.INTERNAL_SERVER_ERROR);
+            result.setMessage(ex.getMessage());
+            result.setResultObj(new ExceptionResult().setStackTrace(ex.getStackTrace()));
+            ex.printStackTrace();
+            return ResponseEntity.internalServerError().body(result);
+        }
+    }
+
     private void Init(){
         result = new AjaxResult();
         helper = new SQLiteHelper(Setup.database_path);
